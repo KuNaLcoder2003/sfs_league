de851584328e191e49705e918ccb72c08a65016c
Kunal Singh <kunalindia59@gmail.com>
2026-04-08 13:13:57 +0530

    New api routes

--- backend/src/routes/over.ts
+++ backend/src/routes/over.ts
@@ -4,17 +4,75 @@ import type { New_Over_Request_Body } from "../types.js";
 
 const router = Router()
 
+const MAX_OVERS: number = 2;
+
 router.post('/newOver', async (req: Request, res: Response) => {
     try {
-        const { bowler_id, match_id, innings_id, over_number } = req.body as New_Over_Request_Body
-        if (!bowler_id || !match_id || !innings_id || !over_number) {
+        const { bowler_id, match_id, innings_id } = req.body as New_Over_Request_Body
+        if (!bowler_id || !match_id || !innings_id) {
             res.status(400).json({
-                message: "Bad request"
+                message: "Bad request",
+                valid: false
             })
             return
         }
-    } catch (error) {
+        const result = await prisma.$transaction(async (tx) => {
+            let task_success: boolean = false
+            const overs = await tx.overs.findMany({
+                where: {
+                    innings_id: Number(innings_id)
+                }
+            })
 
+            if (overs.length == MAX_OVERS) {
+                const runs = ([...overs].reduce((a, b) => a + b.total_runs, 0)) * 2
+                const wickets = [...overs].reduce((a, b) => a + b.wickets, 0)
+                let final_runs: number = runs - (wickets * 2)
+                await tx.score.update({
+                    where: {
+                        innings_id: Number(innings_id)
+                    },
+                    data: {
+                        wickets: wickets,
+                        runs: final_runs,
+                        overs: overs.length
+                    }
+                })
+            }
+            const new_over = await tx.overs.create({
+                data: {
+                    bowler_id: Number(bowler_id),
+                    innings_id: Number(innings_id),
+                    match_id: Number(match_id),
+                    over_no: overs.length + 1
+                }
+            })
+            if (!new_over) {
+                task_success = false
+            }
+            else {
+                task_success = true
+            }
+            return { task_success }
+        })
+        if (!result.task_success) {
+            res.status(403).json({
+                message: "Unable to start new over",
+                valid: false
+            })
+            return
+        }
+        res.status(200).json({
+            message: "New Over started",
+            valid: true
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            message: "Something went wrong",
+            valid: false,
+            error: error
+        })
     }
 })
 
--- backend/src/types.ts
+++ backend/src/types.ts
@@ -24,3 +24,17 @@ export type New_Over_Request_Body = {
     bowler_id: string,
     over_number: number
 }
+
+export type New_Ball_Request_Body = {
+    innings_id: string,
+    match_id: string,
+    batsmen_on_strike: string,
+    batsmen_on_non_strike: string,
+    over_id: string,
+    ball_number: number,
+    ball_type: "FAIR_BALL" | "WIDE" | "NO_BALL" | "DEAD_BALL",
+    is_boundary: boolean,
+    is_wicket: boolean,
+    runs: number,
+    extras: number
+}
